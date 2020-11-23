458d4d18b46ec5bd9f7dc64327972721efe242a5
Felix Pieschka <49276377+Felix-Pi@users.noreply.github.com>
2020-11-23 17:48:47 +0100

    added Tower funktions, added brick functions

--- src/TowerStack.cpp
+++ src/TowerStack.cpp
@@ -8,15 +8,12 @@
 
 TowerStack::TowerStack(LedControl lc) : lc(lc) {
     int devices = lc.getDeviceCount();
-
-    this->max_height = 8 * devices;
 }
 
 
 void TowerStack::setLed(int row, int column, bool state) {
-
     int device;
-    if (row < 0 || row >= this->max_height) return;
+    if (row < 0 || row >= MAX_HEIGHT || column < 0 || column > 7) return;
 
     if (row >= 0 && row < 8) {
         device = 3;
@@ -37,4 +34,100 @@ void TowerStack::setLed(int row, int column, bool state) {
     lc.setLed(device, row, column, state);
 }
 
+int TowerStack::getTowerHeight() {
+    return height;
+}
+
+int TowerStack::incrementHeight() {
+    if (height == 31) return -1;
+    return ++height;
+}
+
+void TowerStack::setBrickSize(int brickSize) {
+    brick_size = brickSize;
+}
+
+int TowerStack::getBrickSize() {
+    return brick_size;
+}
+
+
+void TowerStack::drawTower() {
+    for (int row = 0; row < getTowerHeight(); ++row) {
+        for (int column = 0; column < 8; ++column) {
+            setLed(column, row, tower[row][column] == 1);
+        }
+    }
+}
+
+void TowerStack::start() {
+    setBrickAnimationSpeed(300);
+    drawTower();
+
+    generateBrick();
+}
+
+void TowerStack::generateBrick() {
+    int tower_height = incrementHeight();
+    int brick[8];
+
+    for (int i = 0; i < 8; i++) {
+        int temp = 0;
+        if (i < getBrickSize()) {
+            temp = 1;
+        }
+        tower[tower_height - 1][i] = temp;
+    }
+}
+
+int TowerStack::getBrickAnimationSpeed() const {
+    return brick_animation_speed;
+}
+
+void TowerStack::setBrickAnimationSpeed(int brickAnimationSpeed) {
+    brick_animation_speed = brickAnimationSpeed;
+}
+
+void TowerStack::animateBrick() {
+    int size = getBrickSize();
+    int height = getTowerHeight() - 1;
+
+    //left -> right
+    for (int i = 0; i < 8 - size; ++i) {
+        delay(getBrickAnimationSpeed());
+        int tail = i;
+        int head = i + size;
+
+        tower[height][tail] = 0;
+        tower[height][head] = 1;
+        drawBrick();
+    }
+
+    //right -> left
+    for (int i = 7 - size; i >= 0; --i) {
+        delay(getBrickAnimationSpeed());
+        int tail = i + size;
+        int head = i;
+
+        tower[height][tail] = 0;
+        tower[height][head] = 1;
+        drawBrick();
+    }
+
+}
+
+void TowerStack::drawBrick() {
+    int row = getTowerHeight() - 1;
+
+    for (int column = 0; column < 8; ++column) {
+        setLed(column, row, tower[row][column] == 1);
+        Serial.print(tower[row][column]);
+    }
+    Serial.println();
+}
+
+
+void TowerStack::test() {
+    animateBrick();
+}
 
--- src/TowerStack.h
+++ src/TowerStack.h
@@ -4,16 +4,58 @@
 
 #include <LedControl.h>
 
+#define MAX_HEIGHT 32
+#define COLUMNS 8
+
 class TowerStack {
     LedControl lc;
+
 
 public:
     TowerStack(LedControl lc);
 
-    int max_height;
+    void start();
+
+    void test();
+
+
+private:
+    int height = 4;
+    int tower[MAX_HEIGHT][COLUMNS] = {
+            {0, 1, 1, 1, 1, 1, 1, 0},
+            {0, 1, 1, 1, 1, 1, 1, 0},
+            {0, 1, 1, 1, 1, 1, 1, 0},
+            {0, 0, 1, 1, 1, 1, 0, 0}
+    };
+
+    int brick_animation_speed;
+public:
+    int getBrickAnimationSpeed() const;
+
+    void setBrickAnimationSpeed(int brickAnimationSpeed);
 
 private:
 
+    int brick_size = 5;
+
+    void setBrickSize(int brickSize);
+
+    int getBrickSize();
+
+
+    void drawTower();
+
+    void generateBrick();
+
+    void animateBrick();
+
+    void drawBrick();
+
+    void moveBrick();
+
+    int getTowerHeight();
+
+    int incrementHeight();
 
     void setLed(int row, int column, bool state);
 
