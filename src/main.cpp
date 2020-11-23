8cbf01973c08d3e1eaf479deb18fd8867894a990
Felix Pieschka <49276377+Felix-Pi@users.noreply.github.com>
2020-11-23 19:36:40 +0100

    added main

---
+++ src/main.cpp
@@ -0,0 +1,53 @@
+#include <Arduino.h>
+#include <LedControl.h>
+#include <TowerStack.h>
+
+#include <Wire.h>
+#include <pt.h>
+
+
+#define LCLOADPIN 10 // LC LOAD pin
+#define LCCLKPIN 11 // LC CLK pin
+#define LCDINPIN 12 // LC DataIn pin
+
+#define BUTTON_PIN 2
+
+LedControl lc = LedControl(LCDINPIN, LCCLKPIN, LCLOADPIN, 4);
+
+TowerStack tower(lc);
+
+int previous = HIGH;
+int reading;
+int state = LOW;
+
+void check_for_button_press() {
+    Serial.println("check_for_button_press()");
+
+    int reading = digitalRead(BUTTON_PIN);
+
+    if (reading == HIGH && previous == LOW) {
+        Serial.println("button pressed");
+        tower.button_pressed();
+    }
+    previous = reading;
+}
+
+void setup() {
+    Serial.begin(9600);
+
+    pinMode(BUTTON_PIN, INPUT);
+
+    for (int address = 0; address < lc.getDeviceCount(); address++) {
+        lc.shutdown(address, false);
+        lc.setIntensity(address, 0);
+        lc.clearDisplay(address);
+    }
+
+    tower.start();
+}
+
+
+void loop() {
+    check_for_button_press();
+    //tower.test();
+}
